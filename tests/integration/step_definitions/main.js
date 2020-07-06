930cd6243112a2450382fd464f6257098fdb8513
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2020-07-06 22:43:35 +0100

    updating tests to new cuycumber-js version

--- tests/integration/step_definitions/main.js
+++ tests/integration/step_definitions/main.js
@@ -4,19 +4,16 @@
 const apickli = require('apickli');
 const {defineSupportCode} = require('cucumber');
 
-defineSupportCode(function({Before}) {
-    Before(function() {
-        this.apickli = new apickli.Apickli('https', 'path.to.your.api.endpoint.com/v1/customers');
-        this.apickli.addRequestHeader('Cache-Control', 'no-cache');
-    });
+
+const { Before } = require("cucumber");
+
+Before(function() {
+    this.apickli = new apickli.Apickli('https', 'amer-mint-partner04-dev.apigee.net/v1/customers');
+    this.apickli.addRequestHeader('Cache-Control', 'no-cache');
 });
 
-//// example of adding custom step definitions
-// defineSupportCode( ({Given, When, Then}) => {
-//     Given( () => {
-//         // add custom step dafinitions for Given
-//     });
-// });
+//custom step definitions go here
+
 
 // add the step definitions defined in default apickli gherkin
 // see all default dafinitions here: https://github.com/apickli/apickli#gherkin-expressions
