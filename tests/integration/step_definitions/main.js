22826528cf389fcca410e7bdb0883dcba9a983f9
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2020-07-07 12:04:21 +0100

    unused references

--- tests/integration/step_definitions/main.js
+++ tests/integration/step_definitions/main.js
@@ -2,13 +2,10 @@
 'use strict';
 
 const apickli = require('apickli');
-const {defineSupportCode} = require('cucumber');
-
-
 const { Before } = require("cucumber");
 
 Before(function() {
-    this.apickli = new apickli.Apickli('https', 'amer-mint-partner04-dev.apigee.net/v1/customers');
+    this.apickli = new apickli.Apickli('https', 'your.api.domain.name/v1/customers');
     this.apickli.addRequestHeader('Cache-Control', 'no-cache');
 });
 
