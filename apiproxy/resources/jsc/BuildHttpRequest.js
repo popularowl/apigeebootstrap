73f3812b3f3d21677e562ecfc1cb0a9616240b53
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2020-07-21 15:49:22 +0100

    extending eslint config to google presets. fixing linting errors in JS extentions

--- apiproxy/resources/jsc/BuildHttpRequest.js
+++ apiproxy/resources/jsc/BuildHttpRequest.js
@@ -1,18 +1,15 @@
 // build the party manager data lookup requests
 
 try {
+  var serviceUrl = 'https://owerwritten-target-url.net';
 
-    var serviceUrl = 'https://owerwritten-target-url.net';
-
-    // set the request parameters
-    context.setVariable("request.verb", "GET");
-    context.setVariable("target.url", ''+serviceUrl);
-
+  // set the request parameters
+  context.setVariable('request.verb', 'GET');
+  context.setVariable('target.url', ''+serviceUrl);
 } catch (error) {
-    
-    // error handling framework
-    context.setVariable("customers.error.stackTrace", ""+error);
-    context.setVariable("customers.error.code", "500");
-    context.setVariable("customers.error.message", "Internal Error"); 
-    throw new Error(); // trigger the FaultRules
+  // error handling framework
+  context.setVariable('customers.error.stackTrace', ''+error);
+  context.setVariable('customers.error.code', '500');
+  context.setVariable('customers.error.message', 'Internal Error');
+  throw new Error(); // trigger the FaultRules
 }
--- apiproxy/resources/jsc/HandleErrorResponses.js
+++ apiproxy/resources/jsc/HandleErrorResponses.js
@@ -1,23 +1,18 @@
 // if response code is not 200
 // we raise specific error types
 
-var responseCode = context.getVariable("response.status.code");
+var responseCode = context.getVariable('response.status.code');
 
-if (responseCode == "400"){
-    
-    context.setVariable("customers.error.code", "400");
-    context.setVariable("customers.error.message", "Not Supported Request");    
-    throw new Error(); // trigger the FaultRules
-} 
-else if (responseCode == "404"){
-    
-    context.setVariable("customers.error.code", "404");
-    context.setVariable("customers.error.message", "Not Found");  
-    throw new Error(); // trigger the FaultRules
-}
-else if (responseCode == "500") {
-    
-    context.setVariable("customers.error.code", "500");
-    context.setVariable("customers.error.message", "Internal Error"); 
-    throw new Error(); // trigger the FaultRules
+if (responseCode == '400') {
+  context.setVariable('customers.error.code', '400');
+  context.setVariable('customers.error.message', 'Not Supported Request');
+  throw new Error(); // trigger the FaultRules
+} else if (responseCode == '404') {
+  context.setVariable('customers.error.code', '404');
+  context.setVariable('customers.error.message', 'Not Found');
+  throw new Error(); // trigger the FaultRules
+} else if (responseCode == '500') {
+  context.setVariable('customers.error.code', '500');
+  context.setVariable('customers.error.message', 'Internal Error');
+  throw new Error(); // trigger the FaultRules
 }
